1f63c1c62f9b3048fb5b05c79f0dc517a274a139
daiyichen27 <yichendai3@gmail.com>
2026-03-23 13:40:20 -0700

    Misc. - removed deprecated function

--- app/src/main/java/com/example/fridgecheck/ui/MainActivity.kt
+++ app/src/main/java/com/example/fridgecheck/ui/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.fridgecheck.ui
 
 import android.Manifest
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -284,12 +284,19 @@ class MainActivity : ComponentActivity() {
                                             ownedIngredients = selectedIngredients,
                                             onClick = {
                                                 val uri = recipe.url.toUri()
-                                                val builder = CustomTabsIntent.Builder()
 
-                                                builder.setToolbarColor("#4CAF50".toColorInt())
-                                                builder.setShowTitle(true)
+                                                // 1. Create the color scheme (Non-deprecated way)
+                                                val defaultColors = CustomTabColorSchemeParams.Builder()
+                                                    .setToolbarColor("#4CAF50".toColorInt()) // Edamam Green
+                                                    .build()
 
-                                                val customTabsIntent = builder.build()
+                                                // 2. Build the Intent with the color scheme
+                                                val customTabsIntent = CustomTabsIntent.Builder()
+                                                    .setDefaultColorSchemeParams(defaultColors)
+                                                    .setShareState(CustomTabsIntent.SHARE_STATE_ON) // Adds a share button inside the browser!
+                                                    .setShowTitle(true)
+                                                    .build()
+
                                                 customTabsIntent.launchUrl(context, uri)
                                             }
                                         )
